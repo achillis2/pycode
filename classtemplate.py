83dfb0c999182dc678d7272bcc7686a4147b79fe
Ding Li <dli@nexant.com>
2019-06-23 16:44:56 -0500

    add 01.py

---
+++ classtemplate.py
@@ -0,0 +1,18 @@
+# class MyClass:
+#     variable = "blah"
+#
+#     def function(self):
+#         print("This is a message inside the class.")
+#
+# myobjectx = MyClass()
+#
+# myobjectx.function()
+#
+
+
+class Solution(object):
+    def function(self, s, numRows):
+
+        return final_string
+
+print Solution().function("PAYPALISHIRING",3)
